d56d258106833c6c7b08f51a7db5fd5fd345e9a3
kanatapple <edamame1224@gmail.com>
2020-10-27 17:59:31 +0900

    add hello world

---
+++ hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\.ghq\github.com\kanatapple\rust-study\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\.ghq\github.com\kanatapple\rust-study\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+C:\.ghq\github.com\kanatapple\rust-study\hello-world\target\debug\hello-world.exe: C:\.ghq\github.com\kanatapple\rust-study\hello-world\src\main.rs
