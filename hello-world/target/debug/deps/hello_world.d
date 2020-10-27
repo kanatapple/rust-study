e5e3da01c7407612a2b2bf6a384c01c0030d8988
kanatapple <edamame1224@gmail.com>
2020-10-27 18:01:36 +0900

    ignore target

--- hello-world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\.ghq\github.com\kanatapple\rust-study\hello-world\target\debug\deps\hello_world.exe: src\main.rs
-
-C:\.ghq\github.com\kanatapple\rust-study\hello-world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-C:\.ghq\github.com\kanatapple\rust-study\hello-world\target\debug\hello-world.exe: C:\.ghq\github.com\kanatapple\rust-study\hello-world\src\main.rs
